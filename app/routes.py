875b743ede90692844db08afa2336ea72783f797
EC2 Default User <ec2-user@ip-172-31-40-72.us-east-2.compute.internal>
2019-07-30 19:38:28 +0000

    Webpages with stocks with shares and final balance

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,25 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+    
+@app.route('/shares', methods = ['GET', 'POST'])
+def shares():
+    if request.method == "GET":
+        return "Please fill out the form."
+    else:
+        userdata = dict(request.form)
+        all_shares = model.shares_calculator(userdata['symbol'], float(userdata['Balance']), userdata['yearbuying'])
+        return render_template('shares.html', all_shares = all_shares, initial_year = userdata['yearbuying'], initial_balance = float(userdata['Balance']), symbol = userdata['symbol'])
+        
+
+@app.route('/finalbalance', methods = ['GET', 'POST'])
+def finalbalance():
+    if request.method == "GET":
+        return "Please fill out the form."
+    else:
+        userdata = dict(request.form)
+        total_sharesfinal = float(userdata['all_shares'])
+        print("Ok")
+        final_balance = model.balance_calculator(total_sharesfinal, float(userdata['shares']), userdata['symbol'], float(userdata['initial_balance']), userdata['initial_year'], userdata['yearselling'])
+        return render_template('finalbalance.html', finalbalance = final_balance, symbol = userdata['symbol'])
